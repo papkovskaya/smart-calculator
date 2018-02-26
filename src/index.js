ce1c12ddebf16f66f47d9a4eb3a8539a62af3a1f
Olga Papkovskaya <o.papkovskaya@gmail.com>
2018-02-26 14:13:56 +0300

    must be fixedgit add src/index.js! to many errors

--- src/index.js
+++ src/index.js
@@ -1,27 +1,93 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.result = initialValue;
+    this.array = [];
+    this.sign = [];
+    this.array.push(this.result);
   }
 
   add(number) {
-    // your implementation
+    this.array.push(number);
+    this.sign.push('+');
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.array.push(number);
+    this.sign.push('-');
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.array.push(number);
+    this.sign.push('*');
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.array.push(number);
+    this.sign.push('/');
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.array.push(number);
+    this.sign.push('**');
+    return this;
   }
+
+  toString(){
+    while (this.sign.length != 0) {
+      for (let i = 0; i < this.sign.length; i++) {
+        if (this.sign[i] == '**') {
+          let a = this.array[i];
+          let b = this.array[i + 1];
+          let res = this.array[i] ** this.array[i + 1];
+          this.sign.splice(i, 1);
+          this.array[i] = res;
+          this.array.splice(i + 1, 1);
+        }
+      }
+      for (let i = 0; i < this.sign.length; i++) {
+        if (this.sign[i] == '*') {
+          let a = this.array[i];
+          let b = this.array[i + 1];
+          let res = this.array[i] * this.array[i + 1];
+          this.sign.splice(i, 1);
+          this.array[i] = res;
+          this.array.splice(i + 1, 1);
+        }
+        if (this.sign[i] == '/') {
+          let a = this.array[i];
+          let b = this.array[i + 1];
+          let res = this.array[i] / this.array[i + 1];
+          this.sign.splice(i, 1);
+          this.array[i] = res;
+          this.array.splice(i + 1, 1);
+        }
+      }
+      for (let i = 0; i < this.sign.length; i++) {
+        if (this.sign[i] == '+') {
+          let a = this.array[i];
+          let b = this.array[i + 1];
+          let res = this.array[i] + this.array[i + 1];
+          this.sign.splice(i, 1);
+          this.array[i] = res;
+          this.array.splice(i + 1, 1);
+        }
+        if (this.sign[i] == '-') {
+          let a = this.array[i];
+          let b = this.array[i + 1];
+          let res = this.array[i] - this.array[i + 1];
+          this.sign.splice(i, 1);
+          this.array[i] = res;
+          this.array.splice(i + 1, 1);
+        }
+      }
+    }
+    this.result = this.array[0];
+    return this.result;
+  } 
 }
 
 module.exports = SmartCalculator;
