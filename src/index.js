414daa97f69829501508b82e665ef3f4f427c124
Olga Papkovskaya <o.papkovskaya@gmail.com>
2018-02-27 04:24:40 +0300

    wtf

--- src/index.js
+++ src/index.js
@@ -2,90 +2,100 @@ class SmartCalculator {
   constructor(initialValue) {
     this.result = initialValue;
     this.array = [];
+    this.output = [];
     this.sign = [];
     this.array.push(this.result);
   }
 
   add(number) {
+    this.array.push('+');
     this.array.push(number);
-    this.sign.push('+');
     return this;
   }
   
   subtract(number) {
+    this.array.push('-');
     this.array.push(number);
-    this.sign.push('-');
     return this;
   }
 
   multiply(number) {
+    this.array.push('*');
     this.array.push(number);
-    this.sign.push('*');
     return this;
   }
 
   devide(number) {
+    this.array.push('/');
     this.array.push(number);
-    this.sign.push('/');
     return this;
   }
 
   pow(number) {
+    this.array.push('**');
     this.array.push(number);
-    this.sign.push('**');
     return this;
   }
 
   toString(){
-    while (this.sign.length != 0) {
-      for (let i = 0; i < this.sign.length; i++) {
-        if (this.sign[i] == '**') {
-          let a = this.array[i];
-          let b = this.array[i + 1];
-          let res = this.array[i] ** this.array[i + 1];
-          this.sign.splice(i, 1);
-          this.array[i] = res;
-          this.array.splice(i + 1, 1);
-        }
-      }
-      for (let i = 0; i < this.sign.length; i++) {
-        if (this.sign[i] == '*') {
-          let a = this.array[i];
-          let b = this.array[i + 1];
-          let res = this.array[i] * this.array[i + 1];
-          this.sign.splice(i, 1);
-          this.array[i] = res;
-          this.array.splice(i + 1, 1);
-        }
-        if (this.sign[i] == '/') {
-          let a = this.array[i];
-          let b = this.array[i + 1];
-          let res = this.array[i] / this.array[i + 1];
-          this.sign.splice(i, 1);
-          this.array[i] = res;
-          this.array.splice(i + 1, 1);
-        }
-      }
-      for (let i = 0; i < this.sign.length; i++) {
-        if (this.sign[i] == '+') {
-          let a = this.array[i];
-          let b = this.array[i + 1];
-          let res = this.array[i] + this.array[i + 1];
-          this.sign.splice(i, 1);
-          this.array[i] = res;
-          this.array.splice(i + 1, 1);
-        }
-        if (this.sign[i] == '-') {
-          let a = this.array[i];
-          let b = this.array[i + 1];
-          let res = this.array[i] - this.array[i + 1];
-          this.sign.splice(i, 1);
-          this.array[i] = res;
-          this.array.splice(i + 1, 1);
+    for (let i = 0; i < this.array.length; i++) {
+      if (typeof (this.array[i]) == "number") {
+        this.output.push(this.array[i]);
+      } else {
+        if (this.sign.length == 0) {
+          this.sign.push(this.array[i]);
+        } else {
+          if (this.array[i] == '**') {
+            this.sign.push(this.array[i]);
+          }
+          if (this.array[i] == '*' || this.array[i] == '/') {
+            if (this.sign[this.sign.length - 1] == '+' || this.sign[this.sign.length - 1] == '-') {
+              this.sign.push(this.array[i]);
+            } else if (this.sign[i - 1] == '**' || this.sign[i - 1] == '*' || this.sign[i - 1] == '/') {
+              this.output.push(this.sign.pop());
+              this.sign.push(this.array[i]);
+            }
+          }
+          if (this.array[i] == '+' || this.array[i] == '-') {
+            this.output.push(this.sign.pop());
+            this.sign.push(this.array[i]);
+          }
         }
       }
     }
-    this.result = this.array[0];
+    while (this.sign.length != 0){
+      this.output.push(this.sign.pop());
+    }
+    this.array = [];
+    console.log(this.output);
+    this.output = this.output.reverse();
+
+    while (this.output.length != 0){
+      let a = this.output.pop();
+      if (typeof(a) != "number"){
+        let b2 = this.array.pop();
+        let b1 = this.array.pop();
+        if (a == '**') {
+          this.array.push(b1 ** b2);
+        }
+        if (a == '*') {
+          this.array.push(b1 * b2);
+        }
+        if (a == '/') {
+          this.array.push(b1 / b2);
+        }
+        if (a == '+') {
+          this.array.push(b1 + b2);
+        }
+        if (a == '-') {
+          this.array.push(b1 - b2);
+        }
+      } else {
+        this.array.push(a);
+      }
+      
+    }
+    this.result = this.array.pop();
     return this.result;
   } 
 }
